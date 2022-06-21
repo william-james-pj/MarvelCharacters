5c9a9bf4167a0ebf2ed9caed8c4c6e0c7ff3dd2b
wspr <wspr@gft.com>
2022-06-21 13:25:21 -0300

    Initial Commit

---
+++ Marvel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marvel
+//
+//  Created by Pinto Junior, William James on 21/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
